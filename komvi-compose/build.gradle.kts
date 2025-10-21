659a3fdc66dfcbab5949c1f07411e8ac143e2d5a
jaewoong <ys05059@gmail.com>
2025-10-21 15:07:26 +0900

    refactor: Apply convention plugins to all modules

--- komvi-compose/build.gradle.kts
+++ komvi-compose/build.gradle.kts
@@ -1,35 +1,10 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    id("komvi.android.library")
+    id("komvi.publishing")
 }
 
 android {
     namespace = "io.github.wooongyee.komvi.compose"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
--- komvi-processor/build.gradle.kts
+++ komvi-processor/build.gradle.kts
@@ -1,13 +1,4 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
-}
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+    id("komvi.kotlin.library")
+    id("komvi.publishing")
 }
