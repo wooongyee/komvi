a2dd857fd9a56580db69b44ca93d4340986e2b73
jaewoong <ys05059@gmail.com>
2025-10-21 16:06:44 +0900

    feat: Add MviContainerHost interface with intent shortcut
      - Provide shortcut properties for state and sideEffect
      - Add intent() function shortcut for cleaner syntax

---
+++ komvi-core/src/main/kotlin/io/github/wooongyee/komvi/core/MviContainerHost.kt
@@ -0,0 +1,45 @@
+package io.github.wooongyee.komvi.core
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * Interface for classes that host an [MviContainer].
+ *
+ * Typically implemented by ViewModel classes to provide convenient
+ * access to state and side effects.
+ *
+ * @param S The type of [ViewState]
+ * @param E The type of [SideEffect]
+ */
+interface MviContainerHost<S : ViewState, E : SideEffect> {
+
+    /**
+     * The MVI container instance.
+     */
+    val container: MviContainer<S, E>
+
+    /**
+     * Shortcut property for accessing current state.
+     * Delegates to [MviContainer.state].
+     */
+    val state: StateFlow<S>
+        get() = container.state
+
+    /**
+     * Shortcut property for accessing side effects.
+     * Delegates to [MviContainer.sideEffect].
+     */
+    val sideEffect: Flow<E>
+        get() = container.sideEffect
+
+    /**
+     * Executes an intent block.
+     * Delegates to [MviContainer.intent].
+     *
+     * @param block The intent processing block with [IntentScope] as receiver
+     */
+    fun intent(block: suspend IntentScope<S, E>.() -> Unit) {
+        container.intent(block)
+    }
+}
