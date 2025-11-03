68ae469ffc295bff44e818b329ce21439abf8d4c
jaewoong <ys05059@gmail.com>
2025-11-03 10:50:49 +0900

    fix: Use AGP official publishing API and fix Gradle lifecycle issues
      - Use singleVariant().withSourcesJar() for Android libraries (AGP 7.1+)
      - Wrap extension access in afterEvaluate for proper initialization
      - Remove manual sourcesJar task to avoid conflicts with AGP
      - Fix SourceSetContainer access using getByType<T>()"

--- build-logic/convention/src/main/kotlin/AndroidLibraryPublishingConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryPublishingConventionPlugin.kt
@@ -1,4 +1,4 @@
-import com.android.build.gradle.LibraryExtension
+import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
@@ -6,8 +6,7 @@ import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.get
-import org.gradle.jvm.tasks.Jar
-import org.gradle.kotlin.dsl.register
+import org.gradle.kotlin.dsl.getByType
 
 class AndroidLibraryPublishingConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -16,9 +15,12 @@ class AndroidLibraryPublishingConventionPlugin : Plugin<Project> {
                 apply("maven-publish")
             }
 
-            val sourcesJar = tasks.register<Jar>("sourcesJar") {
-                archiveClassifier.set("sources")
-                from(extensions.getByType(LibraryExtension::class.java).sourceSets["main"].java.srcDirs)
+            extensions.configure<LibraryExtension> {
+                publishing {
+                    singleVariant("release") {
+                        withSourcesJar()
+                    }
+                }
             }
 
             afterEvaluate {
@@ -26,7 +28,6 @@ class AndroidLibraryPublishingConventionPlugin : Plugin<Project> {
                     publications {
                         create<MavenPublication>("release") {
                             from(components["release"])
-                            artifact(sourcesJar)
 
                             groupId = property("library.group").toString()
                             artifactId = project.name
